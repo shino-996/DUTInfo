40d372344da8a6c9eedfb5ce791849a3882a26cb
shino-996 <shino@shino.space>
2018-03-31 22:18:07 +0800

    Course 协议添加了协认 init

--- DUTInfoCore/CourseType.swift
+++ DUTInfoCore/CourseType.swift
@@ -16,6 +16,12 @@ public protocol CourseType {
     init(name: String, teacher: String, time: [TimeType])
 }
 
+extension CourseType {
+    init(name: String, teacher: String, time: [TimeType]) {
+        self.init(name: name, teacher: teacher, time: time)
+    }
+}
+
 public protocol CourseTimeType {
     var place: String { get set }
     var startSection: Int { get set }
@@ -23,4 +29,10 @@ public protocol CourseTimeType {
     var week: Int { get set }
     var teachWeek: [Int] { get set }
     init(place: String, startSection: Int, endSection: Int, week: Int, teachWeek: [Int])
+}
+
+extension CourseTimeType {
+    init(place: String, startSection: Int, endSection: Int, week: Int, teachWeek: [Int]) {
+        self.init(place: place, startSection: startSection, endSection: endSection, week: week, teachWeek: teachWeek)
+    }
 }
