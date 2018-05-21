e42e0126d39a7b8caebe0aab7f2308c0927324ed
shino-996 <shino@shino.space>
2018-05-21 10:00:24 +0800

    修改了时间格式

--- DUTInfoCore/LibrarySite.swift
+++ DUTInfoCore/LibrarySite.swift
@@ -10,7 +10,7 @@ import Fuzi
 import PromiseKit
 
 extension DUTInfo {
-    public func libraryInfo() -> JSON {
+    func libraryInfo() -> JSON {
         var value = ""
         let semaphore = DispatchSemaphore(value: 0)
         let queue = DispatchQueue(label: "libraryinfo")
@@ -18,20 +18,25 @@ extension DUTInfo {
             let url = URL(string: "http://202.118.72.80/web/opentime_show.asp")!
             let request = URLRequest(url: url)
             return URLSession.shared.dataTask(.promise, with: request)
-        }.map(on: queue, String.init).map(on:queue) { str in
-            let htmlStr = try! HTMLDocument(string: str)
-            var infoStr = htmlStr.body!.stringValue.trimmingCharacters(in: .whitespacesAndNewlines).split(separator: "：")
-            _ = infoStr.removeFirst()
-            if infoStr.count == 3 {
-                let tmp = infoStr[1].split(separator: "-")
-                let startDateStr = String(infoStr[0]) + ":" + String(tmp[0])
-                let endDateStr = String(tmp[1]) + ":" + String(String(infoStr[2]).split(separator: "\r\n")[0])
-                let library = Info.Library(open: startDateStr,
-                                      close: endDateStr)
-                let encoder = JSONEncoder()
-                let jsonData = try! encoder.encode(library)
-                value = String(data: jsonData, encoding: .utf8)!
+        }.map(on: queue, String.init)
+        .done(on:queue) { str in
+            guard let html = try? HTMLDocument(string: str) else {
+                throw DUTError.htmlError
             }
+            let pnode = html.xpath("/html/body/p")
+            if pnode.count != 2 {
+                throw DUTError.htmlError
+            }
+            let timeArray = pnode[1].stringValue.filter {
+                ([((0 ... 9).map { "\($0)" }), ["-", "："]].flatMap { $0 }).contains(String($0))
+            }.split {
+                $0 == "-" || $0 == "："
+            }
+            let library = Info.Library(open: timeArray[0] + ":" + timeArray[1],
+                                       close: timeArray[2] + ":" + timeArray[3])
+            let encoder = JSONEncoder()
+            let jsonData = try! encoder.encode(library)
+            value = String(data: jsonData, encoding: .utf8)!
         }.ensure(on: queue) {
             semaphore.signal()
         }.catch(on: queue) { error in
